a977f7bc383fc5e0e524c0eb311e2e398a8d4c98
Prasanna <gramopadhye37@gmail.com>
2022-07-18 20:17:53 +0530

    Navbar CSS changed to make it mobile responsive

--- src/components/Nav/NavLink.jsx
+++ src/components/Nav/NavLink.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { NavContext } from '../../context/NavContext';
 
-const NavLink = ({ navLinkId, scrollToId }) => {
+const NavLink = ({ navLinkId, scrollToId, onClick }) => {
 	const { activeNavLinkId, setActiveNavLinkId } = useContext(NavContext);
 
 	const handleClick = () => {
 		setActiveNavLinkId(navLinkId);
 		document.getElementById(scrollToId).scrollIntoView({ behavior: 'smooth' });
+		onClick()
 	};
 
 	return (
@@ -14,7 +15,7 @@ const NavLink = ({ navLinkId, scrollToId }) => {
 			id={navLinkId}
 			className={activeNavLinkId === navLinkId ? 'activeClass nav-links' : 'nav-links'}
 			onClick={handleClick}
-		
+
 		>
 			{navLinkId}
 		</li>
