09978abcab3e26928b04d7e49ac4bf127a77b56e
Prasanna <gramopadhye37@gmail.com>
2022-07-23 20:32:28 +0530

    Navbar changes and updated documentation page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,19 +1,43 @@
 
 import "./Nav.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import NavLink from './NavLink';
 import { navLinks } from './navLinks';
 
 function Navbar() {
+  const [y, setY] = useState(window.scrollY);
+  const [status, setStatus] = useState(false);
   const [active, setActive] = useState(false);
 
   const handleClick = () => {
     setActive(!active);
   };
+
+  const changeStatus = () => {
+    setStatus(!status)
+  }
+
+  const handleNavigation = (e) => {
+    const window = e.currentTarget;
+    if (y > window.scrollY) {
+      setStatus(false)
+    } else if (y < window.scrollY) {
+      setStatus(true)
+    }
+    setY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => handleNavigation(e));
+
+    return () => { // return a cleanup function to unregister our function since its gonna run multiple times
+      window.removeEventListener("scroll", (e) => handleNavigation(e));
+    };
+  }, [y])
   return (
     <>
-      <nav className="navbar">
+      <nav className={`navbar ${status ? 'navbar-open' : 'navbar-close'}`}>
         <div className="navbar-inner-container">
           <h1 className="navbar-logo">
             <a href="/">
@@ -28,7 +52,7 @@ function Navbar() {
 
           {navLinks.map(({ navLinkId, scrollToId }, idx) => (
 
-            <NavLink onClick={handleClick} key={idx} navLinkId={navLinkId} scrollToId={scrollToId} className="navlink" />
+            <NavLink changeStatus={changeStatus} onClick={handleClick} key={idx} navLinkId={navLinkId} scrollToId={scrollToId} className="navlink" />
           ))}
 
         </ul>
